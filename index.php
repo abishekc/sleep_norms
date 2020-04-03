77821463daccb6212460a66160038670b3395ef7
abishekc <acemaster36@gmail.com>
2020-04-03 15:58:51 -0700

    finalized unit change and updated stylesheet

--- index.php
+++ index.php
@@ -18,12 +18,12 @@
                 var value = this.value;
                 if (value == "imperial"){
                     console.log("imperial updated");
-                    $('#weight_heading').text("What is your weight? (lbs)");
-                    $('#height_heading').text("What is your height? (inches)");
+                    $('#weight_units').text("POUNDS");
+                    $('#height_units').text("INCHES");
                 } else if(value == "metric"){
                     console.log("metric updated");
-                    $('#weight_heading').text("What is your weight? (kgs)");
-                    $('#height_heading').text("What is your height? (cm)");
+                    $('#weight_units').text("KILOGRAMS");
+                    $('#height_units').text("CENTIMETERS");
                 }
             });
         });
@@ -32,18 +32,8 @@
 
 <div id = "compass">
 	<div id = "left">
-		<form id="input_param" action="data.php" method="post">
-            <h2>What is your ethnicity?</h2>
-			<select name="ethnicity" id="ethnicity_input">
-			    <option value="white">White</option>
-			    <option value="black">Black</option>
-			    <option value="asian">Asian</option>
-			    <option value="hispanic">Hispanic</option>
-			    <option value="other">Other</option>
-			    <option value="prefer">Prefer Not to Say</option>
-			</select><br>
-            
-			<h2>What is your age?</h2>
+		<form id="input_param" action="data.php" method="post">            
+			<h2 style="margin-bottom:10px;">What is your age?</h2>
 			<div id="age_div">
 				<input id="age_input" type="text" name="age"><br>
 			</div>
@@ -56,20 +46,33 @@
 				<input type="radio" name="sex" value="female" id="gender_female"/>
 				<label for="gender_female"><img src="img/female_v2.png" id="label_female"/></label>
 			</div><br>
+            
+            <h3>PREFERRED UNITS</h2>
+            <select name="metric" id="unit_input">
+			    <option value="metric">metric</option>
+			    <option value="imperial">imperial</option>
+			</select><br>
 
-			<h2 id="weight_heading">What is your weight? (kg)</h2>
+			<h2 id="weight_heading">What is your weight?</h2>
+            <h3 id="weight_units">KILOGRAMS</h3>
 			<div id="weight_div">
 				<input id="age_input" type="text" name="weight"><br>
 			</div>
 
-			<h2 id="height_heading">What is your height? (cm)</h2>
+			<h2 id="height_heading">What is your height?</h2>
+            <h3 id="height_units">CENTIMETERS</h3>
 			<div id="height_div">
-				<input id="age_input" type="text" name="height"><br>
+				<input id="age_input" type="text" name="height">
 			</div>
-
-			<select name="metric" id="unit_input">
-			    <option value="metric">metric</option>
-			    <option value="imperial">imperial</option>
+            
+            <h2>What is your ethnicity?</h2>
+			<select name="ethnicity" id="ethnicity_input">
+			    <option value="white">White</option>
+			    <option value="black">Black</option>
+			    <option value="asian">Asian</option>
+			    <option value="hispanic">Hispanic</option>
+			    <option value="other">Other</option>
+			    <option value="prefer">Prefer Not to Say</option>
 			</select><br>
 
 			<input type="submit" value="SUBMIT">
